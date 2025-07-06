04e0baf3de0b8a662deacb6e99b6be9c0bccc6f7
Max-hong <max.hong@techworkslabs.com>
2025-07-06 16:03:30 +0800

    Install TailwindCss & FontaweSome icon lib
    --Init tailwindcss config
    --Import used icons

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faBars, faXmark, faHouse, faCircleInfo, faPencil, faMagnifyingGlass,
+  faGreaterThan, faLessThan, faCircleExclamation
+} from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+library.add(faBars, faXmark, faHouse, faCircleInfo, faPencil, faMagnifyingGlass, faGreaterThan, faLessThan, faCircleExclamation)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/2025-07-04-remote-test/' : '/',
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
