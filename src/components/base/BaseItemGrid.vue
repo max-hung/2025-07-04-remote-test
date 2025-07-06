85efa59a1421052cf803f5b39c48ce8b27fa41fb
Max-hong <max.hong@techworkslabs.com>
2025-07-06 16:10:09 +0800

    Testing demo
    - Design default layout header / main / footer
    - Customize style var
    - useTheme control theme style color

---
+++ src/components/base/BaseItemGrid.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineProps } from "vue"
+defineProps<{ items: string[] }>()
+</script>
+<template>
+  <div class="grid grid-cols-2 p-4">
+    <div v-for="(item, index) in items" :key="index" class="bg-zinc-800 text-center py-10 border border-gray-500">
+      {{ item }}
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,49 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue'
+import AsideMenu from '@/components/aside/AsideMenu.vue'
+import BaseActionBar from '@/components/base/BaseActionBar.vue'
+import BaseItemGrid from '@/components/base/BaseItemGrid.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import { useTheme } from '@/composables/useTheme'
+const { setTheme } = useTheme()
+const items = reactive(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'])
+const menuItem: { label: string; icon: [string, string] }[] = [
+  { label: 'Home', icon: ['fas', 'house'] },
+  { label: 'About', icon: ['fas', 'circle-exclamation'] },
+  { label: 'Post1', icon: ['fas', 'pencil'] },
+  { label: 'Post2', icon: ['fas', 'pencil'] },
+  { label: 'Post3', icon: ['fas', 'pencil'] },
+]
+const actionItems = reactive(['ALL', 'TYPE1', 'TYPE2', 'TYPE3', 'TYPE4'])
+const isShowMenu = ref(false)
+
+function toggleMenu() {
+  isShowMenu.value = !isShowMenu.value
+}
 </script>
-
 <template>
-  <main>
+  <DefaultLayout>
+    <template #header>
+      <div class="text-white font-bold flex items-center justify-between h-12">
+        <div class="flex-1 h-12 bg-[#2f4f4f] flex items-center justify-center border border-gray-500">
+          Change Color
+        </div>
+        <div class="flex">
+          <button class="btn-switch btn-none-switch" @click="setTheme('none')"></button>
+          <button class="btn-switch btn-blue-switch" @click="setTheme('blue')"></button>
+        </div>
+      </div>
+    </template>
 
-  </main>
+    <template #aside>
+      <AsideMenu :menuItems="menuItem" :isShow="isShowMenu" @close="toggleMenu" />
+    </template>
+    <div class="z-30 flex flex-col transition-transform duration-300" :class="{ 'translate-x-52': isShowMenu }">
+      <div class="sticky top-0 z-40">
+        <BaseActionBar :typeList="actionItems" @toggle="toggleMenu" />
+      </div>
+      <BaseItemGrid :items="items" />
+    </div>
+
+  </DefaultLayout>
 </template>
